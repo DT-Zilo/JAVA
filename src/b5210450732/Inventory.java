45aa7ccfd01bbaa88a5fdde46f9d226273a3154c
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 01:09:45 +0700

    1

--- src/b5210450732/Inventory.java
+++ src/b5210450732/Inventory.java
@@ -1,28 +1,28 @@
 package b5210450732;
 
-//5210450732 Rudshahrun Vilailuck
+public class Inventory 
+{
+    public static void main (String[] args)
+    {
+        int productCode = 1001;
+        productName[30] string; // Error
+        int productStock;
+        int i;
 
-public class Inventory {
-	public static void main (String[] args){
-		int productCode = 1001;
-		productName[30] string;
-		int productStock;
-		int i;
-		
-		productCode = new productCode();
-		productName = new productName();
-		
-		for(i=1;i<4;i++){
-		System.out.printIn("Code: ",+productCode);
-		System.out.printIn("Product: ",+productName);
-		System.out.printIn("Stocks: "+productStock);
-		}
-		
-class productCode{
-	int a = 1001;
-	int i;
-}
-		
-	}
+        productCode = new productCode(); // Error
+        productName = new productName(); // Error
 
+        for(i=1;i<4;i++)
+        {
+            System.out.printIn("Code: ",+productCode); // Error
+            System.out.printIn("Product: ",+productName); // Error
+            System.out.printIn("Stocks: "+productStock); // Error
+        }
+		
+        class productCode
+        {
+                int a = 1001;
+                int i;
+        }
+    }
 }
