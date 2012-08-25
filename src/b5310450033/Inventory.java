f0757558018d80bb1086472a516e7196c9a75f14
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 13:04:05 +0700

    ตรวจแล้ว เพิ่มขึ้น 35

--- src/b5310450033/Inventory.java
+++ src/b5310450033/Inventory.java
@@ -2,47 +2,51 @@ package b5310450033;
 
 import java.util.Scanner;
 
+public class Inventory 
+{
+    Scanner sc = new Scanner(System.in);
 
-public class Inventory {
-	Scanner sc = new Scanner(System.in);
-	
-	int[] code = new int[3] ;
-	
-	
-	String name;
-	int total ;
-	
-	
-	public Inventory(String x,int y){
-		code[0]=1001;
-		this.name = x;
-		this.total = y;
-			for(int i=0;i<y;i++){
-				code[i]= code[0]+i;
-				System.out.println(code[i]);
-			}
-	}
-	
-	public String callname(){
-		return this.name;
-	}
-	
-	public String editname(){
-		this.name = sc.next();
-		return this.name;
-	
-	}
-	public int edittotal(){
-		int addtotal = sc.nextInt();
-		this.total = this.total+addtotal;
-		return this.total;
-
-	}
-	public int calltotal(){
-		return this.total;
-	}
-
-	
+    int[] code = new int[3]; // 2 point
 
 
+    String name; // 6 point
+    int total ; // 6 point
+
+
+    public Inventory(String x,int y)
+    {
+        code[0] = 1001; // 2 point
+        
+        this.name = x; // 6 point
+        this.total = y; // 6 point
+        
+        for(int i=0;i<y;i++)
+        {
+            code[i]= code[0]+i; // 0 คะแนน ไม่มีทางกำหนดค่าได้อย่างถูกต้อง
+            System.out.println(code[i]);
+        }
+    }
+
+    public String callname() // 13 point
+    {
+        return this.name;
+    }
+
+    public String editname() // 4.33333333333333333333 point
+    {
+        this.name = sc.next();
+        return this.name;
+    }
+    public int edittotal() // 5 point
+    {
+        int addtotal = sc.nextInt();
+        this.total = this.total+addtotal;
+        
+        return this.total;
+    }
+    
+    public int calltotal() // 13 point
+    {
+        return this.total;
+    }
 }
