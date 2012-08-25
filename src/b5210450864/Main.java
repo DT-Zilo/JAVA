2fc5b679e77b6dfa6900b196bd85a01aedf46693
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 13:06:13 +0700

    ตรวจแล้ว เท่าเดิม

--- src/b5210450864/Main.java
+++ src/b5210450864/Main.java
@@ -12,7 +12,6 @@ public class Main
 
     public static void main(String[] args) 
     {
-        
         public callMethod() // Method ซ้อน Method เตือนไปหลายครั้งมาก
         {
             nameInventory name = new nameInventory; // Error
