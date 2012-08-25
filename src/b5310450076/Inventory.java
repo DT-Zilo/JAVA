73168e45043a374ab6857e33332589959a028c42
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 13:16:37 +0700

    ตรวจแล้ว เท่าเดิม

--- src/b5310450076/Inventory.java
+++ src/b5310450076/Inventory.java
@@ -3,53 +3,56 @@ package b5310450076;
 
 import java.util.Scanner;
 
-public class Inventory {
-	public Scanner sc ;
-	public String NameProduct;
-    public int Idproduct;
+public class Inventory 
+{
+    public Scanner sc ;
+    public String NameProduct; // 6 point
+    public int Idproduct; // 6 point
     public int i,x;
     public int array[];
-    public int num = 0;
-	public Inventory  (){
-		for(int i=0;i<=3;i++);
-		{ 
-		String array[]= 
-		ArrayList<String> = new <String>(System.in);	
-		System.out.println("IdProduct = ");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("NameProduct = ");
-		Scanner sc = new Scanner(System.in);
-		
-		
-	}
-	   for(int i=0;i<=3;i++);
-	   {
-		System.out.println   
-	   }
-	   
-		
-	}
+    public int num = 0; // 6 point
 	
-	public void GetNameproduct (){
+    public Inventory  () // 0 point
+    {
+        for(int i=0;i<=3;i++);
+        { 
+            String array[] = ArrayList<String> = new <String>(System.in);	
+            System.out.println("IdProduct = ");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("NameProduct = ");
+            Scanner sc = new Scanner(System.in); 
+        }
+	   
+        for(int i=0;i<=3;i++);
+        {
+            System.out.println // Error
+        }	
+    }
+	
+    public void GetNameproduct () // 0 point
+    {
+        
+    }
+	
+    public void GetNumberProduct() // 0 point
+    {
+        x = sc.nextInt();
+    }
+	
+    public void EditProduct() // 0 point
+    {
 		
-		
-		
-		
-	}
-	public void GetNumberProduct(){
-		x = sc.nextInt();
-	}
-	public void EditProduct(){
-		
-	}
-	public void AddProduct(){
-		
-	}
-    public void PrintResult(){
-    	
+    }
+	
+    public void AddProduct() // 0 point
+    {
+
+    }
+    
+    public void PrintResult() // 0 point
+    {
     	for(int i=0;i<=3;i++);
     
     	System.out.println(array []);
-	   
-   }
+    }
 }
--- src/b5310450076/Main.java
+++ src/b5310450076/Main.java
@@ -1,27 +1,30 @@
+// Attribute        18
+// Constructor      0
+// Methods          0
+// Main Program     0
+//
+// Total            18#
+
 package b5310450076;
 
 import java.util.Scanner;
 
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
-		
-	String sc = new String();	
-	Inventory aa = new Inventory();
-	
-	aa.GetNameproduct();
-	aa.GetNumberProduct();
-	aa.EditProduct();
-	aa.AddProduct();
-	aa.PrintResult();
-	Scanner eiei = new Scanner(System.in);
-	
-	
-	
-	
-	
-	
-	}
+    public static void main(String[] args) 
+    {
+
+        String sc = new String();	
+        Inventory aa = new Inventory();
+
+        aa.GetNameproduct();
+        aa.GetNumberProduct();
+        aa.EditProduct();
+        aa.AddProduct();
+        aa.PrintResult();
+        Scanner eiei = new Scanner(System.in);
+    }
 
 }
