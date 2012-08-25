34d909704abc079ddcfa443c11af3761bdf58d27
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 15:31:19 +0700

    ตรวจแล้ว เพิ่ม 71

--- src/b5310450092/Inventory.java
+++ src/b5310450092/Inventory.java
@@ -1,34 +1,37 @@
 package b5310450092;
 
-public class Inventory {
+public class Inventory 
+{
+    private String name; // 6 point
+    private int count; // 6 point
+    int id; // 6 point
 
-private String name;
-private int count;
-int id;
+    public Inventory(String name,int count,int id)
+    {
+        this.id=id; // 2 point
+        this.name = name; // 6 point
+        this.count = count; // 6 point
+    }
 
-public Inventory(String name,int count,int id){
-	this.id=id;
-	this.name = name;
-	this.count = count;
-	
-}
+    public String outName() // 13 point
+    {
+        return this.name; 
+    }
 
-public String outName(){
-	return this.name;
-}
+    public void editName(String name) // 13 point
+    {
+        this.name = name;
+        System.out.println("To success.");
+    }
 
-public void editName(String name){
-	this.name = name;
-	System.out.println("To success.");
-}
-
-public void addCount(int countAdd){
-	this.count = this.count+countAdd;
-	System.out.println("This "+this.name+" Outstanding = "+this.count);
-}
-public int showCount(){
-	return this.count;
-}
-
-
+    public void addCount(int countAdd) // 15 point
+    {
+        this.count = this.count+countAdd;
+        System.out.println("This "+this.name+" Outstanding = "+this.count);
+    }
+    
+    public int showCount() // 13 point
+    {
+        return this.count;
+    }
 }
--- src/b5310450092/Main.java
+++ src/b5310450092/Main.java
@@ -1,77 +1,94 @@
+// Attribute        18
+// Constructor      14
+// Methods          54
+// Main Program     100
+//
+// Total            186#
+
 package b5310450092;
 
 import java.util.Scanner;
 
-public class Main {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Inventory[] shop = new Inventory[100];
-		int i=0;
-		int id=1001;
-		System.out.println("+++Inventory Shop+++");
-		System.out.println("1.Input Name");
-		System.out.println("2.Edit Name");
-		System.out.println("3.Edit Count");
-		System.out.println("4.Print Inventory");
-		System.out.println("5.exit");
-		System.out.println("++++++++++++++++++++");
-		while(true){
-		String name,oldname;
-		int count;
-		System.out.print("Input number :");
-		int cho=sc.nextInt();
-		switch (cho){
-		case 1:	System.out.println("+++Input Product+++");
-				System.out.print("Input Product name: ");
-				name = sc.next();
-				System.out.print("Input Initial product: ");
-				count = sc.nextInt();		
-				shop[i] = new Inventory(name, count,id);
-				i++;
-				id++;
-				System.out.println("+++++++++++++++++++");
-				break;
-		case 2: System.out.println("+++Edit name+++");
-				System.out.print("Input old name:");
-				oldname = sc.next();
-				for(int j=0;j<i;j++){
-					if(shop[j].outName().equals(oldname)){
-						System.out.print("Input new name:");
-						name = sc.next();
-						shop[j].editName(name);
-						break;
-					}	
-				
-				}
-				System.out.println("++++++++++++++");
-				break;
-		case 3: System.out.println("+++Edit Count+++");
-				System.out.print("Input name: ");
-				name = sc.next();
-				for(int j=0;j<i;j++){
-					if(shop[j].outName().equals(name)){
-						System.out.print("Input add count: ");		
-						shop[j].addCount(sc.nextInt());
-						break;
-						}
-								
-				}
-				System.out.println("++++++++++++++++");
-				break;
-		case 4: System.out.println("+++Print Inventory+++");
-				
-				for(int j=0;j<i;j++){
-				System.out.println("Product id:"+shop[j].id+" Product name:"+shop[j].outName()+" Outstanding :"+shop[j].showCount());
-				}
-				System.out.println("+++++++++++++++++++++");
-				break;
-		case 5: System.exit(0);
-		
-		}		
-		}
-				
-	}		
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        Inventory[] shop = new Inventory[100];
+        int i=0;
+        int id=1001;
+        System.out.println("+++Inventory Shop+++");
+        System.out.println("1.Input Name");
+        System.out.println("2.Edit Name");
+        System.out.println("3.Edit Count");
+        System.out.println("4.Print Inventory");
+        System.out.println("5.exit");
+        System.out.println("++++++++++++++++++++");
+
+        while(true)
+        {
+            String name,oldname;
+            int count;
+            System.out.print("Input number :");
+            int cho=sc.nextInt();
+
+            switch (cho)
+            {
+                case 1:	System.out.println("+++Input Product+++");
+                        System.out.print("Input Product name: ");
+                        name = sc.next();
+                        System.out.print("Input Initial product: ");
+                        count = sc.nextInt();		
+                        shop[i] = new Inventory(name, count,id); // 40 point
+                        i++;
+                        id++;
+                        System.out.println("+++++++++++++++++++");
+                        break;
+                case 2: System.out.println("+++Edit name+++");
+                        System.out.print("Input old name:");
+                        oldname = sc.next();
+                        
+                        for(int j=0;j<i;j++)
+                        {
+                            if(shop[j].outName().equals(oldname))
+                            {
+                                System.out.print("Input new name:");
+                                name = sc.next();
+                                shop[j].editName(name);
+                                break;
+                            }	
+
+                        }
+                        System.out.println("++++++++++++++");
+                        break;
+                case 3: System.out.println("+++Edit Count+++");
+                        System.out.print("Input name: ");
+                        name = sc.next();
+                        
+                        for(int j=0;j<i;j++)
+                        {
+                            if(shop[j].outName().equals(name))
+                            {
+                                System.out.print("Input add count: ");		
+                                shop[j].addCount(sc.nextInt()); // 40 point
+                                break;
+                            }
+                        }
+                        System.out.println("++++++++++++++++");
+                        break;
+                case 4: System.out.println("+++Print Inventory+++");
+
+                        for(int j=0;j<i;j++)
+                        {
+                            System.out.println("Product id:"+shop[j].id+" Product name:"+shop[j].outName()+" Outstanding :"+shop[j].showCount()); // 20 point
+                        }
+                        System.out.println("+++++++++++++++++++++");
+                        break;
+                case 5: System.exit(0);
+
+            }		
+        }			
+    }		
 }
 
 
