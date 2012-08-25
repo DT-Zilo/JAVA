2366076018c4024a259cb80b3757499756492b11
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 19:26:08 +0700

    ตรวจแล้ว เพิ่ม 12

--- src/b5310450165/Inventory.java
+++ src/b5310450165/Inventory.java
@@ -2,34 +2,39 @@ package b5310450165;
 
 import java.util.Scanner; 
 //ort java.util.ArrayList;
-public class Inventory {
-       public Scanner sc;
-       public String namep;
-       public int num = 0;
-       public int ID = 1001;
-       public int i;
-       
-       public Inventory(){
-    	   for(int i=0;i<3;i++){//name
-               String []arry = new String[3];
-               Scanner sc = new Scanner(System.in);
-               this.namep = sc.next();
-    		   arry[i] = this.namep;
-    	   }
-    	   
-    	   for(int i=0;i<3;i++){
-    		   Integer []ary = new Integer[3];
-    		   ary[i] = 1001+i;
-    		   ID = ary[i];
-    	   }
-    	   
-    	   for(int i=0;i<3;i++){//Count
-    		   Integer [] arr = new Integer[3];
-    		   Scanner sc = new Scanner(System.in);
-    		   arr[i] = sc.nextInt();
-    	     
-    	   }
-       }
+public class Inventory 
+{
+    public Scanner sc;
+    public String namep; // 6 point
+    public int num = 0; // 6 point
+    public int ID = 1001; // 6 point
+    public int i;
+
+    public Inventory() // 0 point
+    {
+        for(int i=0;i<3;i++)
+        {
+            String []arry = new String[3];
+            Scanner sc = new Scanner(System.in);
+            this.namep = sc.next();
+            arry[i] = this.namep;
+        }
+
+        for(int i=0;i<3;i++)
+        {
+            Integer []ary = new Integer[3];
+            ary[i] = 1001+i;
+            ID = ary[i];
+        }
+
+        for(int i=0;i<3;i++)
+        {
+            Integer [] arr = new Integer[3];
+            Scanner sc = new Scanner(System.in);
+            arr[i] = sc.nextInt();
+        }
+    }
+    
 /*public void getName(){
     	 ArrayList<String> arry = new ArrayList<String>(3);
     	 arry.get(0);
@@ -39,9 +44,12 @@ public class Inventory {
   public void editP(){
 	  
   }*/
-    public void printResult(){
-    	   for(int i=0;i<3;i++){
-    	   System.out.println(namep);
-    	   }
+    
+    public void printResult() // 0 point
+    {
+        for(int i=0;i<3;i++)
+        {
+            System.out.println(namep);
+        }
      }
 }
--- src/b5310450165/Main.java
+++ src/b5310450165/Main.java
@@ -1,12 +1,21 @@
+// Attribute        18
+// Constructor      0
+// Methods          0
+// Main Program     0
+//
+// Total            18#
+
 package b5310450165;
 
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
-		Inventory aa = new Inventory();
-	//	aa.getName();
-        aa.printResult();
-	}
+    public static void main(String[] args) 
+    {
+        Inventory aa = new Inventory(); // 0 point
+        //	aa.getName();
+        aa.printResult(); // 0 point
+    }
 
 }
