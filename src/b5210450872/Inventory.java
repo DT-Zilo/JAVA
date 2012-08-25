5cd57da91e0cc828d3656215740e351b6c8665e9
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 12:16:50 +0700

    ตรวจแล้ว

--- src/b5210450872/Inventory.java
+++ src/b5210450872/Inventory.java
@@ -1,44 +1,37 @@
+// Attribute        8
+// Constructor      0
+// Methods          0
+// Main Program     0
+//
+// Total            8#
+
 package b5210450872;
 
+public class Inventory 
+{
+    static int name1; // -------
+    static int name2; // รวม 0 point name is String
+    static int name3; // -------
+    
+    static int code1; // -------
+    static int code2; // รวม 2 point
+    static int code3; // -------
+    
+    static int count; // 6 point
 
-
-public class Inventory {
-	static int name1;
-	static int name2;
-	static int name3;
-	static int code1;
-	static int code2;
-	static int code3;
-	static int count;
-
-	
-	public static void main(String[]arg){
-		 name1 = 'a' ;
-		 name2 = 'b';
-		 name3 = 'c';
-		 code1 = 1001;
-		 code2 = 1002;
-		 code3 = 1003;
-		 count = count++;
-		 System.out.println("Enter your name : ");
-		 System.out.println("Enter your code : ");
-		 System.out.println(count);
-		 
-	}
-
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-  
-     
-	}
+    public static void main(String[]arg)
+    {
+        name1 = 'a';
+        name2 = 'b';
+        name3 = 'c';
+        code1 = 1001;
+        code2 = 1002;
+        code3 = 1003;
+        count = count++;
+        System.out.println("Enter your name : ");
+        System.out.println("Enter your code : ");
+        System.out.println(count);
+    }
+}
      
 
