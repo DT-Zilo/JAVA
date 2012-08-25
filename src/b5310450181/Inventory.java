024b81f6fe3c85f8776be3ee73b5166f0c9168b6
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 20:14:46 +0700

    ตรวจแล้ว เพิ่ม 11.8

--- src/b5310450181/Inventory.java
+++ src/b5310450181/Inventory.java
@@ -1,55 +1,45 @@
 package b5310450181;
 
-
-
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
-public class Inventory {
-	public Scanner sc;
-	public int a;
-	private int charnum;
-	private String charname;
-	public String start;
-	public int i;
-	public String x;	
+public class Inventory 
+{
+    public Scanner sc;
+    public int a; // 6 point
+    private int charnum;
+    private String charname; // 6 point
+    public String start;
+    public int i; // 6 point
+    public String x;	
 	
-	
-	public void Constructor(){
-		int a = sc.nextInt();
-		String [] x = new String[3];
-		int[] y = new int[3];
-		int i=0 ;
-		
-		
 
-			{
-				for (i=0;i<start;i++)
-					Scanner sc = new Scanner(System.in);
-				 System.out.println("name of product" );
-				
-				
-				
-						
-					
-			}
+    public void Constructor() // 0 point
+    {
+        int a = sc.nextInt();
+        String [] x = new String[3]; // 0 point
+        int[] y = new int[3]; // 0 point
+        int i=0 ;
+
+        { // Error
+            for (i=0;i<start;i++)
+                Scanner sc = new Scanner(System.in); 
+            
+            System.out.println("name of product" );
+	} // Error
 		
-		public void Printname ();
-		{
-			for int(i=0;i<start;i++)
-			System.out.println("x.get(i)");
+        public void Printname (); // Error 0 point
+        {
+                for int(i=0;i<start;i++)
+                System.out.println("x.get(i)");
+        }
 		
-		}
-		public void changeName();
-		{
-			System.out.println("what's number do you change?");
-			x = sc.next();
-			
-			
-		}
-		
+        public void changeName(); // Error 0 point
+        {
+            System.out.println("what's number do you change?");
+            x = sc.next();
+
 	}
-	
-
+		
+    }
 }
--- src/b5310450181/Main.java
+++ src/b5310450181/Main.java
@@ -1,15 +1,20 @@
+// Attribute        18
+// Constructor      0
+// Methods          0
+// Main Program     0
+//
+// Total            18#
+
 package b5310450181;
 
 
-public class Main {
-
-	public static void main(String[] args) {
-		Inventory putong = new Inventory();
-		putong.Constructor();
-		putong.Printname();
-		putong.changeName();
-		
-
-	}
-
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Inventory putong = new Inventory();
+        putong.Constructor();
+        putong.Printname();
+        putong.changeName();
+    }
 }
