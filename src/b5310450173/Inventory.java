116de823a4a007fdfa93e6d93227c709a99c3e79
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-25 20:04:52 +0700

    ตรวจแล้ว เพิ่ม 1.66

--- src/b5310450173/Inventory.java
+++ src/b5310450173/Inventory.java
@@ -2,35 +2,42 @@ package b5310450173;
 
 import java.util.Scanner;
 
-public class Inventory {
-	int a ;
-	int b;
-	int c;
-public Inventory(int count,int number1,int sum){
-	a = sum ;
-	b = count;
-	c = number1 ;
+public class Inventory 
+{
+    int a ; // 6 point
+    int b; // 6 point
+    int c; // 0 point
+    
+    public Inventory(int count,int number1,int sum)
+    {
+        a = sum ; // 0 point
+        b = count; // 6 point
+        c = number1; // 6 point
+    }
+    
+    public void Name(String name) // 0 point
+    {
+	for(int i = 0 ; i < 3 ; i++)
+            System.out.println("Name product "+name);
+    }
+    
+    public void Product(String name) // 0 point
+    {
+        System.out.print("Name Again ");
+    }
+    
+    public void Count(int count) // 0 point
+    {
+	for(int i = 0 ; i < 3 ; i++)
+            System.out.println("number "+count);
+    }
 
-}
-public void Name(String name){
+    public void getprint() // 0 point
+    {
 	for(int i = 0 ; i < 3 ; i++)
-		System.out.println("Name product "+name);
-	}
-public void Product(String name){
-	System.out.print("Name Again ");
+            System.out.println("number "+b);
 	
-}
-public void Count(int count){
-	for(int i = 0 ; i < 3 ; i++)
-		System.out.println("number "+count);
-}
-
-public void getprint(){
-	for(int i = 0 ; i < 3 ; i++)
-	System.out.println("number "+b);
-	System.out.println("All product "+a);
-	
-	
-}
+        System.out.println("All product "+a);
+    }
 }
 
