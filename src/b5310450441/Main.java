23deeee96c8c7faa8f688b8d0aabb46e4a988fae
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-26 14:22:52 +0700

    ตรวจแล้ว เพิ่ม 3 คนแนน

--- src/b5310450441/Main.java
+++ src/b5310450441/Main.java
@@ -1,14 +1,22 @@
+// Attribute        10
+// Constructor      0
+// Methods          18
+// Main Program     25
+//
+// Total            53#
+
 package b5310450441;
 import java.util.Scanner;
 
-import Test.lnventory;
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Welcome to vending");
-			lnventory c = new lnventory();
-			c.printname();
-			c.editname();
-			c.Buy();
-			c.Checkstock();
-	}
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Welcome to vending");
+        lnventory c = new lnventory();
+        c.printname(); 
+        c.editname();
+        c.Buy(); // 15 point
+        c.Checkstock(); // 10 point
+    }
 }
--- src/b5310450441/lnventory.java
+++ src/b5310450441/lnventory.java
@@ -1,92 +1,114 @@
 package b5310450441;
 import java.util.Scanner;
 
-public class lnventory {
-	private Scanner sc = new Scanner(System.in);
-	private int code [] = new int[100];
-	private int x = 1000;
-	private String name[] = new String[100];
-	private int count[] = new int[100];
-	private int c;
-	public lnventory(){
-		int i =0;
-		int h=1;
-		while(i<3){
-			System.out.println("Name"); //�����Թ���
-			name[i] =sc.next();
-			code[i] =x+h;
-			System.out.println("Count"); //�ӹǹ�Թ���
-			count[i]=sc.nextInt();	
-			h++;
-			i++;
-		}
-	}
-	public void printname(){
-		int i=0;
-		int o = 1;
-		while(i<3){
-			System.out.println(o+":" + name[i]);
-			o++;
-			i++;
-			
-		}
-	}
-	public void editname(){
-		int j=0;
-		while(j<3){
-			System.out.println("Edit name");
-			String d = sc.next();
-			name[j] = d;
-			j++;
-			
-		}
-	}
-	public void Buy(){
-		int i =0;
-		System.out.println("You want buy"); // ��ͧ��ë���������
-		this.printname();
-		int n = sc.nextInt();
-		System.out.println("how must"); // �Ы����������
-		int s = sc.nextInt();
-			switch (n){
-			case 1:System.out.println("waiting");
-			if(s>count[0]){
-			count[0] = count[0]+s;
-			}
-			else{
-				count[0]=count[0]-s;
-			}
-			break;
-			case 2:System.out.println("waiting");
-			if(s>count[1]){
-				count[1] = count[1]+s;
-				}
-				else{
-					count[1]=count[1]-s;
-				}
-				break;
-			case 3:System.out.println("waiting");
-			if(s>count[2]){
-				count[2] = count[2]+s;
-				}
-				else{
-					count[2]=count[2]-s;
-				}
-				break;
-
-
+public class lnventory 
+{
+    private Scanner sc = new Scanner(System.in);
+    private int code [] = new int[100]; // 2 point
+    private int x = 1000; // 6 point
+    private String name[] = new String[100]; // 2 point
+    private int count[] = new int[100];
+    private int c;
+    
+    public lnventory() // 0 point
+    {
+        int i =0;
+        int h=1;
+        
+        while(i<3)
+        {
+                System.out.println("Name"); //�����Թ���
+                name[i] =sc.next();
+                code[i] =x+h;
+                System.out.println("Count"); //�ӹǹ�Թ���
+                count[i]=sc.nextInt();	
+                h++;
+                i++;
+        }
+    }
 	
-}
-	}
-	public void Checkstock(){
-		System.out.println("Stock");
-		int i =0;
-		System.out.println("Name   code   count");
-		while(i<3){
-		System.out.println(name[i]+" "+code[i]+ " "+count[i]);
-		i++;
-		}
-	}
+    public void printname() // 4.33 point
+    {
+        int i=0;
+        int o = 1;
+
+        while(i<3)
+        {
+                System.out.println(o+":" + name[i]);
+                o++;
+                i++;
+
+        }
+    }
+    
+    public void editname() // 4.33 point
+    {
+        int j=0;
+        
+        while(j<3)
+        {
+            System.out.println("Edit name");
+            String d = sc.next();
+            name[j] = d;
+            j++;
+        }
+    }
+    
+    public void Buy() // 5 point
+    {
+        int i =0;
+        System.out.println("You want buy"); // ��ͧ��ë���������
+        this.printname();
+        int n = sc.nextInt();
+        System.out.println("how must"); // �Ы����������
+        int s = sc.nextInt();
+        
+        switch (n)
+        {
+            case 1: System.out.println("waiting");
+                    if(s>count[0])
+                    {
+                        count[0] = count[0]+s;
+                    }
+                    else
+                    {
+                        count[0]=count[0]-s;
+                    }
+                    break;
+            case 2: System.out.println("waiting");
+                    if(s>count[1])
+                    {
+                        count[1] = count[1]+s;
+                    }
+                    else
+                    {
+                        count[1]=count[1]-s;
+                    }
+                    break;
+            case 3: System.out.println("waiting");
+                    if(s>count[2])
+                    {
+                        count[2] = count[2]+s;
+                    }
+                    else
+                    {
+                        count[2]=count[2]-s;
+                    }
+                    break;
+        }
+    }
+	
+    public void Checkstock() // 4.33 point
+    {
+        System.out.println("Stock");
+        int i =0;
+        System.out.println("Name   code   count");
+        while(i<3)
+        {
+            System.out.println(name[i]+" "+code[i]+ " "+count[i]);
+            i++;
+        }
+    }
 }
 	
 
