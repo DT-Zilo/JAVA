b2b04a356f7a20ad9ba17f8bb96d8c4a450dd604
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-26 12:04:05 +0700

    ตรวจแล้ว ลดลง 15

--- src/b5310450289/Inventory.java
+++ src/b5310450289/Inventory.java
@@ -3,42 +3,34 @@ package b5310450289;
 import java.util.Scanner;
 
 
-public class Inventory {
-	
-	
-	public Inventory(){
-	int[]ID 		= 	new int[100];
-	String[]product	=	new String [3];
-	int[]numpro 	= 	new int[100];
-	int i=0;
-	
+public class Inventory 
+{
+    public Inventory() // 0 point
+    {
+        int[]ID 	= 	new int[100]; 
+        String[]product	=	new String [3]; 
+        int[]numpro 	= 	new int[100]; 
+        int i=0;
 
-	
-		
-		System.out.println("Please Enter ProductName :");
-		Scanner bz	= new Scanner(System.in);
-		product[i] 	= bz.next();
-		ID[i] 		= bz.nextInt();
-		numpro[i] 	= bz.nextInt();
-	
-	
-	
-		System.out.println("Please Change ProductName :");
-		product[i] = bz.next();
-	
-	
-	
-	while(i<=3){
-		product[i] = product[i] + i;
-		i++;
-	
-	}
-	
-	
-		System.out.println("ProductName :" 	+ product[i]);
-		System.out.println("Product :" 		+ numpro[i]);
-	
-	}
+        System.out.println("Please Enter ProductName :");
+        Scanner bz	= new Scanner(System.in);
+        product[i] 	= bz.next();
+        ID[i] 		= bz.nextInt();
+        numpro[i] 	= bz.nextInt();
+
+        System.out.println("Please Change ProductName :");
+        product[i] = bz.next();
+
+        while(i<=3)
+        {
+            product[i] = product[i] + i;
+            i++;
+        }
+
+        System.out.println("ProductName :" 	+ product[i]);
+        System.out.println("Product :" 		+ numpro[i]);
+
+    }
 }
 	
 
--- src/b5310450289/Main.java
+++ src/b5310450289/Main.java
@@ -1,10 +1,18 @@
+// Attribute        0
+// Constructor      0
+// Methods          0
+// Main Program     0
+//
+// Total            0#
+
 package b5310450289;
 
 
-public class Main {
-	public static void Main(String[] args){
-	Inventory bb = new Inventory();
-	bb.Inventory();
-	}
-
+public class Main 
+{
+    public static void Main(String[] args)
+    {
+        Inventory bb = new Inventory();
+        bb.Inventory();
+    }
 }
