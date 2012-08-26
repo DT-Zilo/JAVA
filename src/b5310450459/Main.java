1b5e2116eb0377401dbc8ff4905f7f14f6da782d
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-26 16:25:40 +0700

    ตรวจแล้ว เพิ่ม 15 คะแนน

--- src/b5310450459/Main.java
+++ src/b5310450459/Main.java
@@ -1,19 +1,20 @@
+// Attribute        14
+// Constructor      0
+// Methods          18
+// Main Program     25
+//
+// Total            57#
+
 package b5310450459;
 
 
-public class Main {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	  Inventory call = new Inventory();
-	  call.Constructor();
-	  //call.Addstore();
-	  call.Callstore();
-		
-	  
-
-	}
-
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Inventory call = new Inventory();
+        call.Constructor(); // 15 point
+        //call.Addstore();
+        call.Callstore(); // 10 point
+    }
 }
