0ec4e03c4add9eb27abeb18ebf87427acc82b598
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-26 18:21:58 +0700

    ตรวจแล้ว เพิ่ม 21.33

--- src/b5310450483/Inventory.java
+++ src/b5310450483/Inventory.java
@@ -3,62 +3,73 @@ package b5310450483;
 import java.util.Scanner;
 
 
-public class Inventory {
+public class Inventory 
+{
 
-	private int code;
-	private Scanner sc;
-	private String name1;
-	private String name2;
-	private String name3;
-	private int num =  3;
-	int []namelist = new int [num];
+    private int code; // 6 point
+    private Scanner sc;
+    private String name1; // 2 point
+    private String name2;
+    private String name3;
+    private int num =  3; // 6 point
+    int []namelist = new int [num];
 	
+    public void constructor()
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i=0;i<num;i++)
+        {
+            System.out.println("code namber "+(i+1)+":");
+            code = sc.nextInt();
+        }
+        
+        for(int i=0;i<num;i++)
+        {
+            System.out.println("code namber "+(i+1)+":");
+            name1 = sc.next();
+        }
+        
+        for(int i=0;i<num;i++)
+        {
+            System.out.println("code namber "+(i+1)+":");
+            num = sc.nextInt();
+        }
+    }
 	
-	public void constructor(){
-		Scanner sc = new Scanner(System.in);
-		for(int i=0;i<num;i++){
-			System.out.println("code namber "+(i+1)+":");
-			code = sc.nextInt();
-		}
-		for(int i=0;i<num;i++){
-				System.out.println("code namber "+(i+1)+":");
-			name1 = sc.next();
-		}
-		for(int i=0;i<num;i++){
-					System.out.println("code namber "+(i+1)+":");
-			num = sc.nextInt();
-		}
-	}
+    public void callname()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter "+num+" list");
+        for(int i=0;i<num;i++)
+        {
+            System.out.print("name "+(i+1)+":");
+            name1 = sc.next();
+        }
+    }
 	
-	public void callname(){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter "+num+" list");
-		for(int i=0;i<num;i++){
-			System.out.print("name "+(i+1)+":");
-			name1 = sc.next();
-		}
-		}
-	public void edit(){
-		Scanner aa = new Scanner(System.in);
-		for(int i=0;i<num;i++){
-			System.out.print("edit name"+(i+1)+":");
-			name2 = aa.next();
-		}
-	}
-	public void add(){
-		Scanner bb = new Scanner(System.in);
-		for(int i=0;i<num;i++){
-			System.out.print("add name"+(i+1)+":");
-			name3 = bb.next();
-		
-	}
-	}
+    public void edit() // 4.33 point
+    {
+        Scanner aa = new Scanner(System.in);
+        for(int i=0;i<num;i++)
+        {
+            System.out.print("edit name"+(i+1)+":");
+            name2 = aa.next();
+        }
+    }
 	
-	public void callnum(){
-		System.out.println("name :"+name3+"");
-	
-		
-		
-		
+    public void add() // 5 point
+    {
+        Scanner bb = new Scanner(System.in);
+        for(int i=0;i<num;i++)
+        {
+            System.out.print("add name"+(i+1)+":");
+            name3 = bb.next();
 	}
+    }
+	
+    public void callnum() 
+    {
+        System.out.println("name :"+name3+"");
+    }	
 }
--- src/b5310450483/main.java
+++ src/b5310450483/main.java
@@ -1,15 +1,20 @@
+// Attribute        14
+// Constructor      0
+// Methods          9.33
+// Main Program     0
+//
+// Total            23.33#
+
 package b5310450483;
 
-
-public class main {
-
-	
-	public static void main(String[] args) {
-		Inventory a = new Inventory();
-		a.callname();
-		a.edit();
-		a.add();
-		a.callnum();
-	}
-
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        Inventory a = new Inventory();
+        a.callname();
+        a.edit();
+        a.add();
+        a.callnum();
+    }
 }
