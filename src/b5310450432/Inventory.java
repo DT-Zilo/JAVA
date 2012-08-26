8f38a8447167a9f558b0a586ffc1252400b58c02
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-26 12:31:42 +0700

    ตรวจแล้ว เพิ่ม 5 คะแนน

--- src/b5310450432/Inventory.java
+++ src/b5310450432/Inventory.java
@@ -3,43 +3,39 @@ package b5310450432;
 import java.util.Scanner;
 
 
-public class Inventory {
+public class Inventory 
+{
 	
-Scanner sc = new Scanner(System.in);
-int code[] = new int[10];
-String name[]= new String[10];
-int all[] = new int[10];
-int sss[] = new int[10];
-public Inventory( ){
-	
-	
-		
+    Scanner sc = new Scanner(System.in);
+    int code[] = new int[10]; // 2 point
+    String name[]= new String[10]; // 2 point
+    int all[] = new int[10]; // 2 point
+    int sss[] = new int[10];
+    
+    public Inventory( ) // 0 point
+    {	
+    }
+
+    public void kty()
+    {
+	int count[] = new int [10];
+	for(int j=0;j<3;j++)
+        {
+            System.out.print("Enter code :");
+            code[j] = sc.nextInt();
+            System.out.print("Enter name :");
+            name[j] = sc.next();
+            System.out.print("all of stock:");
+            all[j] = sc.nextInt();
+            System.out.print("your chould:");
+            sss[j] = sc.nextInt();
+            count[j] = all[j]-sss[j];
 	}
 	
-	
-	
-
-			
-	
-
-public void kty(){
-	int count[] =new int [10];
-	for(int j=0;j<3;j++){
-		System.out.print("Enter code :");
-		code[j] = sc.nextInt();
-		System.out.print("Enter name :");
-		name[j] = sc.next();
-		System.out.print("all of stock:");
-		all[j] = sc.nextInt();
-		System.out.print("your chould:");
-        sss[j] = sc.nextInt();
-        count[j] = all[j]-sss[j];
-	}
-	for(int k=0;k<3;k++){
-	System.out.println("name ="+name[k]);
-	System.out.println("instock ="+count[k]);
-}
-	}
-
-
+        for(int k=0;k<3;k++)
+        {
+            System.out.println("name ="+name[k]);
+            System.out.println("instock ="+count[k]);
+        }   
+    }
 }
--- src/b5310450432/Main.java
+++ src/b5310450432/Main.java
@@ -1,17 +1,17 @@
+// Attribute        6
+// Constructor      0
+// Methods          0
+// Main Program     30
+//
+// Total            0#
+
 package b5310450432;
 
-
-public class Main {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-Inventory b= new Inventory();
-b.kty();
-
-	}
-
+public class Main 
+{
+    public static void main(String[] args) 
+    {	
+        Inventory b= new Inventory();
+        b.kty();
+    }
 }
