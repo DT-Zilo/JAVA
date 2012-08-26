416aa3ee9b68f6c2ead0e96fad13eb122c7c730f
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-26 12:11:06 +0700

    เหมือนกับ b5310450157

--- src/b5310450327/Main.java
+++ src/b5310450327/Main.java
@@ -1,27 +1,34 @@
+// Attribute        0
+// Constructor      0
+// Methods          0
+// Main Program     0
+//
+// Total            0#
+
 package b5310450327;
 
 import java.util.Scanner;
 
+public class Main 
+{
+    private static int product;
+    private static Scanner sc;
 
-public class Main {
-	private static int product;
-	private static Scanner sc;
+    public static void main(String[] args) 
+    {
+        sc = new Scanner(System.in);
+        System.out.print("Enter Number of Product = ");
+        product = sc.nextInt();
 
-	
-	public static void main(String[] args) {
-		sc = new Scanner(System.in);
-		System.out.print("Enter Number of Product = ");
-		product = sc.nextInt();
-	
-		
-		Inventory m = new Inventory(product);
-		
-		for(int i=0 ; i<product ;i++){
-			m.enter(i);
-			}
-			m.output(product);
-		
-			
-	}
+
+        Inventory m = new Inventory(product);
+
+        for(int i=0 ; i<product ;i++)
+        {
+            m.enter(i);
+        }
+
+        m.output(product);
+    }
 
 }
