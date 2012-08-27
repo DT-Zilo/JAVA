260f1966434b2cf98f5e28e2d2d62819c8072e51
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-27 16:43:16 +0700

    เหมือนกับ b5310450441

--- src/b5310450530/Inventory.java
+++ src/b5310450530/Inventory.java
@@ -3,94 +3,99 @@ package b5310450530;
 import java.util.Scanner;
 
 public class Inventory {
-	 Scanner sc = new Scanner(System.in);
-	 int [] code   = new int[3];
-	 String[] name = new String[3];
-	 int [] count = new int [40];
-	public Inventory(){
-		int i = 0;
-		int k = 1;
-		while(i<3){
-			
-		System.out.println("input code and name ");
-		code[i] = sc.nextInt();
-		name[i] = sc.next();
-		i++;}
-	}
-		//����ª����Թ���
-		public void printname() {
-					int i = 0;
-					while(i<3){
-				System.out.println("name : " +name[i] );
-				i++;
-								}
-		}
-		//�٨ӹǹ�Թ���
-		public void printstock(){
-			int s;
-			System.out.println("input stock");
-			s = sc.nextInt();
-			System.out.println("stock is "+s);
-			
-		}
-		//�����Թ���
-		public void addstock(){
-			
-			int i=0;
-			System.out.println("add stock ");
-			this.printname();
-			int n = sc.nextInt();
-			System.out.println("older stock");
-			int s = sc.nextInt();
-			switch(n){
-				case 1 :System.out.println("you want add");
-				if (s>count[0]){
-					count[0] = count[0]+s;
-				}
-				else{
-				count[0] = count[0]-s;
-			}
-				break;
-			 case 2 :System.out.println("you want add");
-				if (s>count[0]){
-					count[1] = count[1]+s;
-				}
-			
-			else{
-				count[1] = count[1]-s;
-			} break;
-			 case 3 :System.out.println("you want add");
-				if (s>count[0]){
-					count[2] = count[2]+s;
-				}
-			
-			else{
-				count[2] = count[2]-s;
-			} break;
-			}			
-		}
-		//������Թ���
-				public void changename(){
-					int k = 0;
-					 
-					while(k<3){
-						System.out.println("change name");
-						String e1 = sc.next();
-						name[k]= e1;
-								k++;
-					}
-					}
-					public void printall(){
-						System.out.println("stock");
-						int i = 0;
-						System.out.println("code");
-						while(i<3){
-							System.out.println(name[i]+" " +code[i]+" "+count[i]);
-							
-							
-						}
-					}
-				
+
+    Scanner sc = new Scanner(System.in);
+    int[] code = new int[3]; // 2 point
+    String[] name = new String[3]; // 2 point
+    int[] count = new int[40];  // 2 point
+
+    public Inventory() {
+        int i = 0;
+        int k = 1;
+        while (i < 3) {
+
+            System.out.println("input code and name ");
+            code[i] = sc.nextInt();
+            name[i] = sc.next();
+            i++;
+        }
+    }
+    //����ª����Թ���
+
+    public void printname() {
+        int i = 0;
+        while (i < 3) {
+            System.out.println("name : " + name[i]);
+            i++;
+        }
+    }
+    //�٨ӹǹ�Թ���
+
+    public void printstock() {
+        int s;
+        System.out.println("input stock");
+        s = sc.nextInt();
+        System.out.println("stock is " + s);
+
+    }
+    //�����Թ���
+
+    public void addstock() {
+
+        int i = 0;
+        System.out.println("add stock ");
+        this.printname();
+        int n = sc.nextInt();
+        System.out.println("older stock");
+        int s = sc.nextInt();
+        switch (n) {
+            case 1:
+                System.out.println("you want add");
+                if (s > count[0]) {
+                    count[0] = count[0] + s;
+                } else {
+                    count[0] = count[0] - s;
+                }
+                break;
+            case 2:
+                System.out.println("you want add");
+                if (s > count[0]) {
+                    count[1] = count[1] + s;
+                } else {
+                    count[1] = count[1] - s;
+                }
+                break;
+            case 3:
+                System.out.println("you want add");
+                if (s > count[0]) {
+                    count[2] = count[2] + s;
+                } else {
+                    count[2] = count[2] - s;
+                }
+                break;
+        }
+    }
+    //������Թ���
+
+    public void changename() {
+        int k = 0;
+
+        while (k < 3) {
+            System.out.println("change name");
+            String e1 = sc.next();
+            name[k] = e1;
+            k++;
+        }
+    }
+
+    public void printall() {
+        System.out.println("stock");
+        int i = 0;
+        System.out.println("code");
+        while (i < 3) {
+            System.out.println(name[i] + " " + code[i] + " " + count[i]);
+
+
+        }
+    }
 }
-		
-	
--- src/b5310450530/main.java
+++ src/b5310450530/main.java
@@ -1,18 +1,22 @@
+// Attribute        12
+// Constructor      12
+// Methods          4.33
+// Main Program     0
+//
+// Total            28.33#
+
 package b5310450530;
+
 import java.util.Scanner;
-import testmid.Inventory;
+
 public class main {
 
-	public static void main(String[] args) {
-		
-		Inventory gg = new Inventory();
-		gg.printname();
-		gg.printstock();
-		gg.addstock();
-		gg.changename();
-			}
-		}
-	
-	
+    public static void main(String[] args) {
 
-
+        Inventory gg = new Inventory();
+        gg.printname();
+        gg.printstock();
+        gg.addstock();
+        gg.changename();
+    }
+}
