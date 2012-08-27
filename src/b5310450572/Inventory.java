7c82c578c6287519867a4e2c61019de4aea3da67
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-27 23:39:40 +0700

    ลดลง 16.33

--- src/b5310450572/Inventory.java
+++ src/b5310450572/Inventory.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Inventory {
 	private Scanner sc;
 	private Scanner String;
-	private String one;
-	private String two;
+	private String one; // 2 point
+	private String two; 
 	private String three;
-	private int num1=0;
+	private int num1=0; // 2 point
 	private int num2=0;
 	private int num3=0;
 	
-	public void name(){
+	public void name(){ // 4.33 point
 		System.out.println("Please input name : ");
 		Scanner sc = new Scanner(System.in);
 		one = sc.next();
@@ -22,7 +22,7 @@ public class Inventory {
 		System.out.println("1002 : "+two);
 		System.out.println("1003 : "+three);
 	}
-	public void delete(){
+	public void delete(){ // 0 point
 		System.out.println("Please new name : ");
 		Scanner sc = new Scanner(System.in);
 		one = sc.next();
@@ -32,7 +32,7 @@ public class Inventory {
 		System.out.println("1002 : "+two);
 		System.out.println("1003 : "+three);
 	}
-	public void input(){
+	public void input(){ // 0 point
 		System.out.println("Input limit 1001,1002,1003 : ");
 		Scanner sc = new Scanner(System.in);
 		num1 = sc.nextInt();
@@ -42,7 +42,7 @@ public class Inventory {
 		System.out.println("1002 : "+two+num2);
 		System.out.println("1003 : "+three+num3);
 	}
-	public void total(){
+	public void total(){ // 4.33 point
 		System.out.println("1001 : " +num1);
 		System.out.println("1002 : " +num2);
 		System.out.println("1003 : " +num3);
--- src/b5310450572/Main.java
+++ src/b5310450572/Main.java
@@ -1,3 +1,10 @@
+// Attribute        4
+// Constructor      0
+// Methods          8.66
+// Main Program     15
+//
+// Total            27.66#
+
 package b5310450572;
 
 
@@ -5,10 +12,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		Inventory nisit = new Inventory();
-		nisit.name();
+		nisit.name(); // 15 point
 		nisit.delete();
 		nisit.input();
-		nisit.total();
+		nisit.total(); // 10 point
 	}
 
 }
