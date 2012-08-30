f9b5ff77d2a97699be214622ac79a6447ee76f5a
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-30 23:58:55 +0700

    Update src/b5410450108/Inventory.java

--- src/b5410450108/Inventory.java
+++ src/b5410450108/Inventory.java
@@ -7,15 +7,15 @@ public class Inventory {
 
 	private Scanner sc;
 	private int code=1001;
-	private String name[];
-	private int amount;
-	private int sum=0;
+	private String name[]; // 2
+	private int amount; // 6
+	private int sum=0; // 6
 	
 	public Inventory(){
 		sc = new Scanner(System.in);
 		for(int i=0;i<=3;i++){
 		System.out.println("Enter Product name");
-		name[i+1] = sc.next();
+		name[i+1] = sc.next(); // error
 		System.out.println("Enter amount of Product");
 		amount =sc.nextInt();
 			sum = amount+sum;
@@ -25,18 +25,18 @@ public class Inventory {
 		
 		
 	}
-	public void Name(){
+	public void Name(){ // 0
 		System.out.println("Product name is "+name);
 	}
-	public void EditName(){
+	public void EditName(){ // 0
 		
 	}
-	public void AmountPlus(){
+	public void AmountPlus(){ // 5
 		int change = sc.nextInt();
 		amount = amount+change;
 		sum = sum+change;
 	}
-	public void Amount(){
+	public void Amount(){ // 0
 		System.out.println("Current amount of all product = "+sum);
 	}
 	
