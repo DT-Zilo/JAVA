875c8fde54ffbeaa9d1a2c18c7dc2f0012958a3b
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-31 10:50:06 +0700

    Update src/b5410450230/Inventory.java

--- src/b5410450230/Inventory.java
+++ src/b5410450230/Inventory.java
@@ -1,23 +1,22 @@
-
 public class Inventory {
-	private int code = 1001;
-	private String name;
-	private int count = 0;
+	private int code = 1001; // 6
+	private String name; // 6
+	private int count = 0; // 6
 	public Inventory(String x,int i,int y){
-		this.code += i;
-		this.name = x;
-		this.count = y;
+		this.code += i; // 6
+		this.name = x; // 6
+		this.count = y; // 6
 	}
-	public void list(){
+	public void list(){ // 13
 		System.out.print("Code : "+code+" | Name : "+name);
 	}
-	public void edit(String newName){
+	public void edit(String newName){ // 13
 		this.name = newName;
 	}
-	public void add(int x){
+	public void add(int x){ // 15
 		this.count += x;
 	}
-	public void stock(){
+	public void stock(){ // 13
 		System.out.println(" | Stock : "+count);
 	}
 }
