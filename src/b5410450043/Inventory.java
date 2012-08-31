4c5131b65cc9a1c1d048be137c40b1eee57ddb32
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-08-31 10:34:56 +0700

    Update src/b5410450043/Inventory.java

--- src/b5410450043/Inventory.java
+++ src/b5410450043/Inventory.java
@@ -4,33 +4,33 @@ import java.util.Scanner;
 
 public class Inventory {
  
-	public int[] code = new int[3],item = new int[3];
-	public String[] nameItem = new String[3];
-	public int number;
+	public int[] code = new int[3],item = new int[3]; // 2
+	public String[] nameItem = new String[3]; // 2
+	public int number; // 6
 	Scanner sc = new Scanner(System.in);
-	public Inventory(){
+	public Inventory(){ // 0
 		code[number] = 1001;
 		item[number] = 0;
 	}
 	
-	public void add(int x){
+	public void add(int x){ 
 		System.out.print("Name : ");
 		nameItem[x] = sc.nextLine();
 		code[x] = 1001+x;
 
 	}
 	
-	public void showName(){
+	public void showName(){ // 4.33
 	 for (int i = 0; i < 3; i++) {
 		 System.out.println("Name : "+nameItem[i]+" Code : "+code[i]+" Stock : "+item[i]);
 		
 	}
  }
-	public void fixName(int i , String x){
+	public void fixName(int i , String x){ // 4.33
 		nameItem[i] = x;
 		
 	}
-	public void addItem(int x,int i){
+	public void addItem(int x,int i){ // 5
 		item[i] = item[i]+x;
 		
 	}
